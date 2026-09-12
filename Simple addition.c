b5acfb954307dfb96859fe9e99a7df560a0f1fc2
CyuH7 <3562026230@qq.com>
2026-09-12 14:31:14 +0800

    简单联系练习1

--- Simple addition.c
+++ Simple addition.c
@@ -6,4 +6,11 @@ int main()
     C=A+B;
     printf("%d",C);
     return 0;
+    char a;
+    scanf("%c",&a);
+    for(int i=1;i<=3;i++){
+    for(int i=1;i<=3;i++){
+    printf("%c",a);}	
+	printf("\n");}
+	return 0;
 }
