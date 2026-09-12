61ca2a3c40b19c9ba3197a9c6f3f44d0229ca5e7
CyuH7 <3562026230@qq.com>
2026-09-12 14:32:40 +0800

    废稿

--- Simple addition.c
+++ Simple addition.c
@@ -1,11 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    int A,B,C;
-    scanf("%d %d",&A,&B);
-    C=A+B;
-    printf("%d",C);
-    return 0;
     char a;
     scanf("%c",&a);
     for(int i=1;i<=3;i++){
