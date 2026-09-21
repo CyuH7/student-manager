c0447c061d77f23d4afe658d6b4c31d9fc35a255
CyuH7 <3562026230@qq.com>
2026-09-21 20:56:01 +0800

    自加自减的短路运算

---
+++ Short-circuit.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    printf("%d\n",i++);
+    printf("%d\n",++i);
+
+    int a=0,b=1;
+    if(a&&b++){
+
+    }
+    printf("%d\n",b);
+
+    if(a||b++){
+
+    }
+    printf("b=%d\n",b);
+
+    return 0;
+}
