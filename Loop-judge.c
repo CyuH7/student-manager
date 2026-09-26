5c0c4b566a3409c125aeeb045ef84aaeb94d6b9b
CyuH7 <3562026230@qq.com>
2026-09-26 17:01:20 +0800

    循环判断

---
+++ Loop-judge.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int isPrime(int n){
+    int i;
+    if(n<=1)return 0;
+    for(i=2;i*i<=n;i++){
+        if(n%i==0)return 0;
+    }
+    return 1;
+}
+
+int main(){
+    int i;
+    for(i=2;i<=100;i++){
+        if(isPrime(i))printf("%d",i);
+    }
+    return 0;
+}
